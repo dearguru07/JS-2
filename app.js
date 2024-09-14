20b0109746c4fbfd22ccdf083b06c0f5058902b2
dearguru07 <pangalaguruyadav101@gmail.com>
2024-09-14 09:18:36 +0530

    'js-2'

---
+++ app.js
@@ -0,0 +1,68 @@
+// console.log("Hello world.......")
+
+// varibles---------
+
+// var num=5
+// // num="giri"
+// var num="Heloo"
+// console.log(num)
+
+
+
+// let num="gurucharan"
+//     // num="hello"
+// let num="Heloo"    
+// console.log(num)
+
+
+
+// const num="gurucharan"
+//     // num="Hello"
+// const num="Heloo"
+//     console.log(num)
+
+
+// ----------------Hosting--------------
+
+
+//--trying to acess the value before initilation, its possible by only var keyword, it can't possible by let, cost 
+
+// console.log(name)
+// var name="Gur"
+
+
+// console.log(name)
+// let name="GuruCharan"
+
+
+// console.log(name)
+// const name="Guru"
+
+
+// --------------------DataTypes-----------------------
+
+// const num=15
+// console.log(num)
+// console.log(typeof(num))
+
+
+// const strng="Guru"
+// console.log(strng)
+// console.log(typeof(strng))
+
+// const name=true
+// console.log(name)
+// console.log(typeof(name))
+
+// const name=false
+// console.log(name)
+// console.log(typeof(name))
+
+
+let array=["Guru","charan","mahes","rahul","ramu"]
+// console.log(array)
+// console.log(array[2])
+// console.log(array.length)
+
+array.pop()
+console.log(array)
